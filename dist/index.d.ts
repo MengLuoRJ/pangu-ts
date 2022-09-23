8732352a7cb8a44e9d33da9c7fba30aa11e59cf6
MengLuoRJ <mengluor@gmail.com>
2022-09-24 04:57:13 +0800

    package into index

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,11 @@
-import pangu from './pangu';
+declare class Pangu {
+    version: string;
+    constructor();
+    convertToFullwidth(symbols: string): string;
+    spacing(text: string): string;
+    spacingText(text: string, callback?: Function): any;
+    spacingTextSync(text: string): string;
+}
+declare const pangu: Pangu;
 export default pangu;
+export { pangu, Pangu };
